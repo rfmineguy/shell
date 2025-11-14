28edfdc73d89aa6692202664c68f9f2db4d1a021
Riley Fischer <rfmineguy@gmail.com>
2025-11-13 23:36:07 -0800

    driver: error check errors from shell_prompt before running the shell_expr

--- src/main.c
+++ src/main.c
@@ -8,9 +8,12 @@ int main(int argc, char **argv) {
     shell_expr expr;
 
     // prompt for input
-    if (shell_prompt(&state, &expr) == -1) continue;
-    shell_expr_debug(expr);
+    if (shell_prompt(&state, &expr) != 0) {
+      fprintf(stderr, "Error: %s at index %d\n", expr.error, expr.errloc);
+      continue;
+    }
 
-    // shell_run_cmd(expr);
+    int exit_code = shell_run_expr(expr);
+    (void)exit_code;
   }
 }
