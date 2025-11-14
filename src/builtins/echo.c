3ae361dd8dfe3d4249dda59e235756baa8f63942
Riley Fischer <rfmineguy@gmail.com>
2025-11-13 22:54:05 -0800

    builtin(echo): test echo builtin by printing the arguments given to it

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,5 +1,10 @@
 #include "../builtins.h"
+#include <stdio.h>
 
 int builtin_echo(int argc, char** argv) {
+  printf("argc: %d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("%d:   %s\n", i, argv[i]);
+  }
   return 0;
 }
